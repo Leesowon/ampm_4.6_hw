c14481bcad5ee4d0c94842a43af0b6623d5d17b1
leesw <leesowon00@jbnu.ac.kr>
2021-04-07 21:59:09 +0900

    inital commit
    Signed-off-by: leesw <leesowon00@jbnu.ac.kr>

---
+++ src/main/java/kr/ac/jbnu/ampm/sowon/SowonApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.sowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SowonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/ampm/sowon/SowonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.sowon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SowonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
