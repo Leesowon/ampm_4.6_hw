80969ba275b20b9be3804d2414f9e4779afb0d29
leesw <leesowon00@jbnu.ac.kr>
2021-05-07 21:01:29 +0900

    ampm hw 0503

--- src/main/java/kr/ac/jbnu/ampm/sowon/Controller.java
+++ src/main/java/kr/ac/jbnu/ampm/sowon/Controller.java
@@ -1,24 +1,46 @@
 package kr.ac.jbnu.ampm.sowon;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@Slf4j
 public class Controller {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<?> responseEntity(HttpServletRequest request){
+    public ResponseEntity<?> responseEntity(HttpServletRequest request, @PathVariable String id) {
         ResponseEntity<?> responseEntity = null;
+        Map<String, Object> voMap = null;
 
-        responseEntity = new ResponseEntity<>("OK", HttpStatus.OK);
+        if (id != null && !id.equals("")) {
+            voMap = new HashMap<String, Object>();
 
-        log.info("GET / method");
+            voMap.put("name", id);
+            voMap.put("age", "22");
+
+            voMap.put("books", new HashMap<String, Object>() {{
+
+                put("book1", "마션");
+                put("book2", "소프트웨어공학개론");
+
+                Map<String, Object> presentMap = new HashMap<String, Object>();
+                presentMap.put("name", "디지털공학개론");
+                put("book3", presentMap);
+
+            }});
+
+            responseEntity = new ResponseEntity<>(voMap, HttpStatus.OK);
+
+        } else {
+            responseEntity = new ResponseEntity<>("NOT_FOUND", HttpStatus.NOT_FOUND);
+        }
 
         return responseEntity;
     }
